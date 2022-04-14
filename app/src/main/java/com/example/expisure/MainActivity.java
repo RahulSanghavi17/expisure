184cebb18ad4e677126a481e01927bd1a245e5b6
RahulSanghavi17 <97745170+RahulSanghavi17@users.noreply.github.com>
2022-04-13 23:48:56 -0400

    added files

--- app/src/main/java/com/example/expisure/MainActivity.java
+++ app/src/main/java/com/example/expisure/MainActivity.java
@@ -1,16 +1,72 @@
 package com.example.expisure;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1  ;
+    Button b2  ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.addexpbt);
+        b1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(MainActivity.this,Expenses.class);
+                        startActivity(i);
+                    }
+
+                }
+        );
+
+        b1 = findViewById(R.id.viewexpbt);
+        b1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(MainActivity.this,records.class);
+                        startActivity(i);
+                    }
+
+                }
+        );
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_Dashboard:
+                        Toast.makeText(MainActivity.this, "Dashboard", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_Settings:
+                        Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_Record:
+                        Toast.makeText(MainActivity.this, "Record", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+
+
 
     }
 }
